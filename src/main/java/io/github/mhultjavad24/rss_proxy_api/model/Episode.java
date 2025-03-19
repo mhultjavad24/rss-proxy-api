fddc44b3d137aa4866d954b6b311d50540312863
Marcus Hultgren <marcus.hultgren@yh.nackademin.se>
2025-03-19 17:14:13 +0100

    Add Episode model class with properties

---
+++ src/main/java/io/github/mhultjavad24/rss_proxy_api/model/Episode.java
@@ -0,0 +1,62 @@
+package io.github.mhultjavad24.rss_proxy_api.model;
+
+import java.util.Date;
+
+public class Episode {
+    private String title;
+    private String link;
+    private String description;
+    private Date pubDate;
+    private String guid;
+
+    public Episode() {
+    }
+
+    public Episode(String title, String link, String description, Date pubDate, String guid) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubDate = pubDate;
+        this.guid = guid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+} 
