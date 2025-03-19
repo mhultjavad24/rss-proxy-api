2448d3132d13652d91fec472fc566d269416d31d
Argus Wake <argus@wake.com>
2025-03-19 16:53:37 +0100

    Initial commit

---
+++ src/main/java/io/github/mhultjavad24/rss_proxy_api/RssProxyApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mhultjavad24.rss_proxy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssProxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssProxyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mhultjavad24/rss_proxy_api/RssProxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mhultjavad24.rss_proxy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssProxyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
